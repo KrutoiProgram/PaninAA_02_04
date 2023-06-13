dabccf48243fc8f7cfb2f39ebd31a00ee2dea5b0
KrutoiProgram <dsfafafasdf@mail.ru>
2023-06-13 13:14:50 +0500

    Добавьте файлы проекта.

---
+++ PaninAA_02_04/CalculatorPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PaninAA_02_04
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CalculatorPage :ContentPage
+    {
+        public CalculatorPage ()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ PaninAA_02_04/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace PaninAA_02_04
+{
+    public partial class MainPage :ContentPage
+    {
+        public MainPage ()
+        {
+            InitializeComponent();   
+            if(Preferences.ContainsKey("Login")&&Preferences.ContainsKey("Password"))
+            {
+                Navigation.PushModalAsync(new CalculatorPage());
+            }
+        }
+
+        private void Click_RegButton (object sender, EventArgs e)
+        {
+            if(!string.IsNullOrWhiteSpace(Login_Entry.Text) && !string.IsNullOrWhiteSpace(Password_Entry.Text))
+            {
+                Preferences.Set("Login",Login_Entry.Text);
+                Preferences.Set("Password", Password_Entry.Text);
+                Navigation.PushModalAsync(new CalculatorPage());
+            }
+            else
+            {
+                DisplayAlert("Ошибка ввода", "Логин и пароль не могут быть пустыми", "OK");
+            }
+        }
+    }
+}
